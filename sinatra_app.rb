6e87b03ca0717231040de0c5d3c85919cdf82b12
Ben Trevor <benjamin.trevor@gmail.com>
2013-05-20 17:01:13 -0500

    Finish registrant validations

--- sinatra_app.rb
+++ sinatra_app.rb
@@ -1,17 +1,27 @@
 require 'sinatra'
 
 class SinatraApp < Sinatra::Base
-  set :registrations, []
+  set :registrants, []
 
   get '/' do
     redirect :register
   end
 
   post '/validate_registration' do
-    if /^[a-z]*$/ =~ params[ :username ]
-      settings.registrations << [ params[ :username ], params[ :email ] ]
-      erb :registration_success
+    if invalid_username?
+      @flash_message = invalid_username_flash_message
+      erb :register
+    elsif invalid_email?
+      @flash_message = invalid_email_flash_message
+      erb :register
+    else
+      settings.registrants << [ params[:username], params[:email] ]
+      redirect '/success'
     end
+  end
+
+  get '/success' do
+    erb :registration_success
   end
 
   get '/leaderboard' do
@@ -20,5 +30,60 @@ class SinatraApp < Sinatra::Base
 
   get '/register' do
     erb :register
+  end
+
+  private
+  def invalid_username?
+    name_regex_fails? or name_too_long? or name_not_unique?
+  end
+
+  def invalid_email?
+    email_regex_fails? or email_not_unique?
+  end
+
+  def name_regex_fails?
+    !( /^[a-zA-Z0-9]+$/ =~ params[:username] )
+  end
+
+  def email_regex_fails?
+    !( /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i =~ params[:email] )
+  end
+
+  def name_not_unique?
+    settings.registrants.each do |registrant|
+      return true if registrant[0] == params[:username]
+    end
+
+    false
+  end
+
+  def email_not_unique?
+    settings.registrants.each do |registrant|
+      return true if registrant[1].downcase == params[:email].downcase
+    end
+
+    false
+  end
+
+  def name_too_long?
+    params[:username].length > 40
+  end
+
+  def invalid_username_flash_message
+    if name_regex_fails?
+      "Username must contain only alphanumeric characters."
+    elsif name_too_long?
+      "Username must be less than 40 characters."
+    elsif name_not_unique?
+      "Username is already taken."
+    end
+  end
+
+  def invalid_email_flash_message
+    if email_regex_fails?
+      "Invalid email."
+    elsif email_not_unique?
+      "Email already taken."
+    end
   end
 end
--- spec/sinatra_app_spec.rb
+++ spec/sinatra_app_spec.rb
@@ -5,31 +5,123 @@ Capybara.app = SinatraApp
 
 describe SinatraApp, type: :feature do
   after :each do
-    SinatraApp.registrations = []
+    SinatraApp.registrants = []
   end
 
-  it "can register a user" do
-    SinatraApp.registrations.length.should be 0
-    register_user "valid", "valid@example.com"
+  describe "successful registration" do
+    it "can register a user" do
+      SinatraApp.registrants.length.should be 0
+      register_user "valid", "valid@example.com"
 
-    SinatraApp.registrations.length.should be 1
+      SinatraApp.registrants.length.should be 1
+    end
+
+    it "adds a user to a registrant list" do
+      register_user "firstname", "firstemail@example.com"
+      register_user "secondname", "secondemail@example.com"
+
+      SinatraApp.registrants[0][0].should == "firstname"
+      SinatraApp.registrants[0][1].should == "firstemail@example.com"
+      SinatraApp.registrants[1][0].should == "secondname"
+      SinatraApp.registrants[1][1].should == "secondemail@example.com"
+    end
+
+    it "redirects to a 'success' page after registration" do
+      register_user "valid", "valid@example.com"
+
+      page.should have_content "Registration successful!"
+    end
+
+    it "accepts names with letters or numbers" do
+      assert_valid_names "hey", "ABC", "123"
+    end
+
+    it "allows valid emails" do
+      assert_valid_email "valid@example.com"
+    end
   end
 
-  it "redirects to a 'success' page after registration" do
-    register_user "valid", "valid@example.com"
+  describe "unsuccessful registration" do
+    it "doesn't accept non-alphanumeric characters" do
+      assert_invalid_characters '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', ']', '{', '}', '\\', '|', ';', ':', "'", '"', ',', '.', '<', '>', '?', '/', '`', '~', '-', '_', '=', '+', 'å', '∫', '∑'
+    end
 
-    page.should have_content "Registration successful!"
+    it "doesn't allow a blank name" do
+      register_user "", "valid@example.com"
+      SinatraApp.registrants.length.should be 0
+    end
+
+    it "limits names to 40 characters" do
+      long_name = "a" * 50
+      register_user long_name, "valid@example.com"
+      SinatraApp.registrants.length.should be 0
+    end
+
+    it "only allows unique usernames" do
+      register_user "valid", "valid@example.com"
+      SinatraApp.registrants.length.should be 1
+      register_user "valid", "differentvalid@example.com"
+      SinatraApp.registrants.length.should be 1
+    end
+
+    it "doesn't allow invalid emails" do
+      assert_invalid_emails "user@foo,com", "user_at_foo.org", "example.user@foo.", "foo@bar_baz.com", "foo@bar+baz.com"
+    end
+
+    it "only allows unique emails" do
+      register_user "valid", "valid@example.com"
+      SinatraApp.registrants.length.should be 1
+      register_user "differentvalid", "valid@example.com"
+      SinatraApp.registrants.length.should be 1
+    end
+
+    it "shows an appropriate flash message for invalid names" do
+      assert_name_flash_message "invalid!", "Username must contain only alphanumeric characters"
+      long_name = 'a' * 50
+      assert_name_flash_message long_name, "Username must be less than 40 characters"
+
+      register_user "taken", "differentvalid@example.com"
+      assert_name_flash_message "taken", "Username is already taken"
+    end
+
+    it "shows an appropriate flash message for invalid emails" do
+      register_user "valid", "@@invalid@@,,,,@,@,"
+      page.should have_content( "Invalid email." )
+
+      register_user "valid", "taken@example.com"
+      register_user "differentvalid", "taken@example.com"
+      page.should have_content( "Email already taken" )
+    end
   end
 
-  it "only accepts alphanumeric characters for the name" do
-    assert_invalid_names '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', ']', '{', '}', '\\', '|', ';', ':', "'", '"', ',', '.', '<', '>', '?', '/', '`', '~', '-', '_', '=', '+'
+  def assert_name_flash_message( name, flash_message )
+    register_user name, "valid@example.com"
+    page.should have_content( flash_message )
   end
 
-  def assert_invalid_names( *characters )
+  def assert_invalid_emails( *emails )
+    emails.each do |email|
+      register_user "valid", email
+      SinatraApp.registrants.length.should be 0
+    end
+  end
+
+  def assert_valid_email( email )
+    register_user "valid", email
+    SinatraApp.registrants.length.should be 1
+  end
+
+  def assert_valid_names( *names )
+    names.each do |name, counter|
+      expect{ register_user name, "valid#{name}@example.com" }.to change{ SinatraApp.registrants.length }.by 1
+    end
+  end
+
+  def assert_invalid_characters( *characters )
     characters.each do |character|
       name = "invalid" << character
       register_user name, "valid@example.com"
-      SinatraApp.registrations.length.should be 0
+      SinatraApp.registrants.length.should be 0
     end
   end
 
